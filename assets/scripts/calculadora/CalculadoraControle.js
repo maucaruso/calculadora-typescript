c131b28f01c5f93f6094b68bc70613a62d4f7ddc
maucaruso <carusojr@outlook.com>
2022-09-04 15:26:03 -0300

    button click method

--- assets/scripts/calculadora/CalculadoraControle.js
+++ assets/scripts/calculadora/CalculadoraControle.js
@@ -4,6 +4,46 @@ export class CalculadoraControle {
     constructor(tela = new Tela()) {
         this.tela = tela;
         new DataHora();
+        this.eventosBotoes();
+    }
+    eventosBotoes() {
+        document.querySelectorAll("#teclado button").forEach(elemento => {
+            elemento.addEventListener("click", (evento) => {
+                const target = evento.target;
+                switch (target.id) {
+                    case "zero":
+                    case "um":
+                    case "dois":
+                    case "tres":
+                    case "quatro":
+                    case "cinco":
+                    case "seis":
+                    case "sete":
+                    case "oito":
+                    case "nove":
+                        this.adicionarNumero(Number(target.dataset.valor));
+                        break;
+                    case "adicao":
+                    case "subtracao":
+                    case "divisao":
+                    case "multiplicacao":
+                        break;
+                    case "ponto":
+                        break;
+                    case "limpar":
+                        break;
+                    case "desfazer":
+                        break;
+                    case "porcentagem":
+                        break;
+                    case "igual":
+                        break;
+                }
+            });
+        });
+    }
+    adicionarNumero(numero) {
+        this.tela.conteudo = numero.toString();
     }
 }
 //# sourceMappingURL=CalculadoraControle.js.map
--- assets/typescript/calculadora/CalculadoraControle.ts
+++ assets/typescript/calculadora/CalculadoraControle.ts
@@ -2,13 +2,67 @@ import { DataHora } from "./DataHora.js";
 import { Tela } from "./Tela.js";
 
 export class CalculadoraControle {
-  
+
   constructor(
     private tela = new Tela()
   ) {
-  
+
     new DataHora();
-    
+
+    this.eventosBotoes();
+
+  }
+
+  eventosBotoes(): void {
+
+    document.querySelectorAll("#teclado button").forEach(elemento => {
+      elemento.addEventListener("click", (evento: Event) => {
+
+        const target = evento.target as HTMLButtonElement;
+
+        switch (target.id) {
+          case "zero":
+          case "um":
+          case "dois":
+          case "tres":
+          case "quatro":
+          case "cinco":
+          case "seis":
+          case "sete":
+          case "oito":
+          case "nove":
+            this.adicionarNumero(Number(target.dataset.valor));
+          break;
+          
+          case "adicao":
+          case "subtracao":
+          case "divisao":
+          case "multiplicacao":
+          break;
+          
+          case "ponto":
+          break;
+          
+          case "limpar":
+          break;
+          
+          case "desfazer":
+          break;
+          
+          case "porcentagem":
+          break;
+          
+          case "igual":
+          break;
+        }
+
+      })
+    });
+
   }
   
+  adicionarNumero(numero: number): void {
+    this.tela.conteudo = numero.toString();
+  }
+
 }
